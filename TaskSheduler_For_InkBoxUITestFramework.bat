0459321e09cb6fb849fd311ad5fdb6dc4a0bb065
Pratheeba Ravichandran <37662066+PratheebaRavi@users.noreply.github.com>
2022-08-09 11:35:04 -0400

    Add files via upload

---
+++ TaskSheduler_For_InkBoxUITestFramework.bat
@@ -0,0 +1,3 @@
+cd C:\Users\IBCK-WL-037\Inkbox_WorkSpace\Inkbox\testing\Inkbox_UITestFramework
+mvn clean test -Dfilename=C:\Users\IBCK-WL-037\Inkbox_WorkSpace\Inkbox\testing\Inkbox_UITestFramework\testng.xml
+pause
